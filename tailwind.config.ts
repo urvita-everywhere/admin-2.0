f428fbace84db594efaa05c07350181cbdfa5c81
David Aragón <davidaragonandrade@gmail.com>
2023-10-18 17:33:27 -0500

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        backgroundApp: "#FCFBF8",
+        orangeApp: "#F79F4D",
+        darkGreenApp: "#2E4233",
+        softGreenApp: "#435B49",
+        lightGreen: "#A4C9A3",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
